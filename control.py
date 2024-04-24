1c394ca6249bdacff65b8a1ac0d01f68029ca249
vero1890 <166828109+vero1890@users.noreply.github.com>
2024-04-24 13:11:02 +0000

    Create control.py

---
+++ control.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+
+right = 4
+middle = 27
+left = 23
+
+kick_array = []
+
+def setup():
+  GPIO.setmode(GPIO.BCM)  # Numbers GPIOs by physical location
+  GPIO.setup(left, GPIO.OUT)
+  GPIO.setup(middle, GPIO.OUT)
+  GPIO.setup(right, GPIO.OUT)
+  GPIO.output(left, GPIO.LOW)
+  GPIO.output(middle, GPIO.LOW)
+  GPIO.output(right, GPIO.LOW)
+
+
+
+def destroy():
+  GPIO.output(left, GPIO.LOW)
+  GPIO.output(middle, GPIO.LOW)
+  GPIO.output(right, GPIO.LOW)
+  GPIO.cleanup()  # Release resource
+
+
+if __name__ == '__main__':  # Program start from here
+  setup()
+  try:
+    loop()
+  except KeyboardInterrupt:
+    destroy()
